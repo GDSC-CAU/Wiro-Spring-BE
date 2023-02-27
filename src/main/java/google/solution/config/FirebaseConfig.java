feaea9e6f184f11e6ea81aa267a298abcbb65e50
Sangwoo Shin <sangwoonoel@naver.com>
2023-02-27 18:13:15 +0900

    feat(FirebaseConfig): create configration class to initialize firebase

---
+++ src/main/java/google/solution/config/FirebaseConfig.java
@@ -0,0 +1,25 @@
+package google.solution.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+    @Bean
+    public FirebaseAuth firebaseAuth() throws IOException {
+        FileInputStream serviceAccount =
+                new FileInputStream("src/main/resources/serviceAccountKey.json");
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+        FirebaseApp.initializeApp(options);
+        return FirebaseAuth.getInstance(FirebaseApp.getInstance());
+    }
+}
