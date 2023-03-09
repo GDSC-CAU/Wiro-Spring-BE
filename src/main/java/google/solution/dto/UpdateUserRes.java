b4352d34cdae9991d468a6e52ced5f32a6db6896
Sangwoo Shin <sangwoonoel@naver.com>
2023-03-09 22:37:44 +0900

    Update UpdateUserRes: Added updateTime field

--- src/main/java/google/solution/dto/UpdateUserRes.java
+++ src/main/java/google/solution/dto/UpdateUserRes.java
@@ -3,11 +3,13 @@ package google.solution.dto;
 public class UpdateUserRes {
 
     private String message;
+    private String updateTime;
 
     public UpdateUserRes() {
     }
 
-    public UpdateUserRes(String message) {
+    public UpdateUserRes(String message, String updateTime) {
         this.message = message;
+        this.updateTime = updateTime;
     }
 }
