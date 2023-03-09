4d0b6e44604cc85e219de82a13a850c2c3caa2bc
Sangwoo Shin <sangwoonoel@naver.com>
2023-03-10 00:03:49 +0900

    Update UpdateUserReq: Replaced constructor code with Lombok annotation

--- src/main/java/google/solution/dto/UpdateUserReq.java
+++ src/main/java/google/solution/dto/UpdateUserReq.java
@@ -1,20 +1,14 @@
 package google.solution.dto;
 
-import lombok.Getter;
+import lombok.*;
 
 @Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UpdateUserReq {
 
     private String id;
     private String nickname;
     private String email;
-
-    public UpdateUserReq() {
-    }
-
-    public UpdateUserReq(String id, String nickname, String email) {
-        this.id = id;
-        this.nickname = nickname;
-        this.email = email;
-    }
 }
