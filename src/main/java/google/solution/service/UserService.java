f2274258b3162bcb2a67a082d67f7d3099b8405b
Sangwoo Shin <sangwoonoel@naver.com>
2023-03-09 22:26:37 +0900

    Update UserService: Added updateUser method

--- src/main/java/google/solution/service/UserService.java
+++ src/main/java/google/solution/service/UserService.java
@@ -1,8 +1,12 @@
 package google.solution.service;
 
+import google.solution.domain.User;
 import google.solution.dto.GetUserRes;
+import google.solution.dto.UpdateUserRes;
 
 public interface UserService {
 
     public GetUserRes getUser(String id) throws Exception;
+
+    public UpdateUserRes updateUser(User user) throws Exception;
 }
