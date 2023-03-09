c5590d375867f70ee51fd2c05038086ab361d746
Sangwoo Shin <sangwoonoel@naver.com>
2023-03-09 21:43:35 +0900

    Update UserController: Added getUser method

--- src/main/java/google/solution/controller/UserController.java
+++ src/main/java/google/solution/controller/UserController.java
@@ -1,6 +1,11 @@
 package google.solution.controller;
 
+import google.solution.dto.GetUserRes;
+import google.solution.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
+    @GetMapping("/getUserInfo/{userId}")
+    ResponseEntity<GetUserRes> getUser(@PathVariable("userId") String userId) {
+        try {
+            GetUserRes userInfo = userService.getUser(userId);
+            return ResponseEntity.ok().body(userInfo);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
