4a79edb0a19af8b2fbf5d2d2a117d40dc81bf803
Sangwoo Shin <sangwoonoel@naver.com>
2023-03-09 22:31:58 +0900

    Update: UserRepository: Added updateUser method

--- src/main/java/google/solution/repository/UserRepository.java
+++ src/main/java/google/solution/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package google.solution.repository;
 
+import google.solution.domain.User;
 import google.solution.dto.GetUserRes;
+import google.solution.dto.UpdateUserRes;
 
 public interface UserRepository {
 
     public GetUserRes getUser(String id) throws Exception;
+
+    public UpdateUserRes updateUser(User user) throws Exception;
 }
