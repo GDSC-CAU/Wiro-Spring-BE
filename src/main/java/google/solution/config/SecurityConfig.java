d81202152fc148684dec41f02f4426d4b487c1be
Sangwoo Shin <sangwoonoel@naver.com>
2023-03-14 16:14:52 +0900

    Added SecurityConfig: create security configurantion class

---
+++ src/main/java/google/solution/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package google.solution.config;
+
+import com.google.firebase.auth.FirebaseAuth;
+import google.solution.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UserService userService;
+
+    private final FirebaseAuth firebaseAuth;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated().and()
+                .addFilterBefore(new JwtFilter(userService, firebaseAuth),
+                        UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling()
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        // 회원가입, 메인페이지, 리소스
+        web.ignoring().antMatchers("/users/")
+                .antMatchers("/")
+                .antMatchers("/resources/**");
+    }
+}
