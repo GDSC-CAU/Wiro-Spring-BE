c0d25a5379724ebf42b81fd97f406f43a782c1b1
Sangwoo Shin <sangwoonoel@naver.com>
2023-03-14 16:43:33 +0900

    Update UserService: add register method

--- src/main/java/google/solution/service/UserService.java
+++ src/main/java/google/solution/service/UserService.java
@@ -1,12 +1,17 @@
 package google.solution.service;
 
+import google.solution.domain.User;
 import google.solution.dto.GetUserRes;
 import google.solution.dto.UpdateUserReq;
 import google.solution.dto.UpdateUserRes;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
 
     public GetUserRes getUser(String id) throws Exception;
 
     public UpdateUserRes updateUser(UpdateUserReq user) throws Exception;
+
+    public User register(String uid, String email, String nickname);
+
 }
--- src/main/java/google/solution/service/UserServiceImpl.java
+++ src/main/java/google/solution/service/UserServiceImpl.java
@@ -1,19 +1,23 @@
 package google.solution.service;
 
+import google.solution.domain.User;
 import google.solution.dto.GetUserRes;
 import google.solution.dto.UpdateUserReq;
 import google.solution.dto.UpdateUserRes;
 import google.solution.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
     @Override
+    @Transactional(readOnly = true)
     public GetUserRes getUser(String id) throws Exception {
         if (userRepository.getUser(id) == null) {
             throw new Exception();
@@ -24,6 +28,19 @@ public class UserServiceImpl implements UserService {
     @Override
     public UpdateUserRes updateUser(UpdateUserReq user) throws Exception {
         return userRepository.updateUser(user);
+    }
+
+    // 등록 코드
+    public User register(String uid, String email, String nickname) {
+        User user = new User(uid, email, nickname);
+
+        // 등록
+        try {
+            userRepository.saveUser(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return user;
     }
 
 }
