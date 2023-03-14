b69258e63909480ca9d492febb4e2632c23a7edb
Sangwoo Shin <sangwoonoel@naver.com>
2023-03-14 16:37:20 +0900

    Added JwtFilter

---
+++ src/main/java/google/solution/filter/JwtFilter.java
@@ -0,0 +1,64 @@
+package google.solution.filter;
+
+import java.io.IOException;
+import java.util.NoSuchElementException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
+
+import google.solution.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.apache.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    // 생성자 지우고 이렇게 @Required로 코드 전환
+    private final UserService userService;
+    private final FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        // get the token from the request
+        FirebaseToken decodedToken;
+        try{
+            String header = RequestUtil.getAuthorizationToken(request.getHeader("Authorization"));
+            decodedToken = firebaseAuth.verifyIdToken(header);
+        } catch (FirebaseAuthException | IllegalArgumentException e) {
+            // ErrorMessage 응답 전송
+            response.setStatus(HttpStatus.SC_UNAUTHORIZED);
+            response.setContentType("application/json");
+            response.getWriter().write("{\"code\":\"INVALID_TOKEN\", \"message\":\"" + e.getMessage() + "\"}");
+            return;
+        }
+
+        // User를 가져와 SecurityContext에 저장한다.
+        try{
+            UserDetails user = userService.loadUserByUsername(decodedToken.getUid());
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                    user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } catch(NoSuchElementException e){
+            // ErrorMessage 응답 전송
+            response.setStatus(HttpStatus.SC_UNAUTHORIZED);
+            response.setContentType("application/json");
+            response.getWriter().write("{\"code\":\"USER_NOT_FOUND\"}");
+            return;
+        }
+        filterChain.doFilter(request, response);
+    }
+}
