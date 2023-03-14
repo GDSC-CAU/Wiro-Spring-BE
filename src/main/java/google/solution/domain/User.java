5648e3d5c8b1f3c10960199ac40e291381ef2c9c
Sangwoo Shin <sangwoonoel@naver.com>
2023-03-14 16:56:46 +0900

    Update User: modify to implement UserDetails

--- src/main/java/google/solution/domain/User.java
+++ src/main/java/google/solution/domain/User.java
@@ -3,11 +3,15 @@ package google.solution.domain;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @NoArgsConstructor
 @Getter
 @Setter
-public class User {
+public class User implements UserDetails {
 
     private String username;
     private String email;
@@ -17,5 +21,40 @@ public class User {
         this.username = username;
         this.email = email;
         this.nickname = nickname;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
     }
 }
