d93e628407ad1320107bf29d6caec1b66f059100
Sangwoo Shin <sangwoonoel@naver.com>
2023-03-14 16:48:54 +0900

    Update UserRepository: add saveUser method

--- src/main/java/google/solution/repository/UserRepository.java
+++ src/main/java/google/solution/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package google.solution.repository;
 
+import google.solution.domain.User;
 import google.solution.dto.GetUserRes;
 import google.solution.dto.UpdateUserReq;
 import google.solution.dto.UpdateUserRes;
@@ -9,4 +10,6 @@ public interface UserRepository {
     public GetUserRes getUser(String id) throws Exception;
 
     public UpdateUserRes updateUser(UpdateUserReq user) throws Exception;
+
+    public String saveUser(User user) throws Exception;
 }
