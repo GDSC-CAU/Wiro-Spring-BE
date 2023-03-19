3480c20ffd076852a35c36f879eea2ad4260e986
Sangwoo Shin <sangwoonoel@naver.com>
2023-03-19 19:51:03 +0900

    Added ChatController: add sendMessage method

---
+++ src/main/java/google/solution/controller/ChatController.java
@@ -0,0 +1,33 @@
+package google.solution.controller;
+
+
+import google.solution.domain.Message;
+import google.solution.dto.UpdateUserReq;
+import google.solution.dto.UpdateUserRes;
+import google.solution.service.UserService;
+import google.util.BaseResponse;
+import google.util.BaseResponseStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/chat")
+public class ChatController {
+
+    private final UserService userService;
+
+    @PostMapping("/sendMessage")
+    public BaseResponse<SendMessageRes> sendMessage(@RequestBody Message message, Authentication authentication) {
+        try {
+            String id = authentication.getName();
+            SendMessageRes sendMessageRes = userService.sendMessage(id,message);
+            return new BaseResponse<>(sendMessageRes);
+        } catch (Exception e) {
+            return new BaseResponse<>(BaseResponseStatus.FAIL);
+        }
+}
