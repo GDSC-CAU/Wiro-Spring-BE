ad212fde4d52378cb7a0a350e2b218c1ad345fdb
Sangwoo Shin <sangwoonoel@naver.com>
2023-03-21 09:45:59 +0900

    Update ChatServiceImpl: implement getChatMessages method

--- src/main/java/google/solution/service/ChatServiceImpl.java
+++ src/main/java/google/solution/service/ChatServiceImpl.java
@@ -2,6 +2,7 @@ package google.solution.service;
 
 import google.solution.domain.Message;
 import google.solution.dto.GetChatContentRes;
+import google.solution.dto.GetChatMessageRes;
 import google.solution.dto.GetChatRoomRes;
 import google.solution.dto.SendMessageRes;
 import google.solution.repository.ChatRepository;
@@ -34,6 +35,12 @@ public class ChatServiceImpl implements ChatService {
     public List<GetChatContentRes> getChatContent(String roomId, String id) throws Exception {
         List<GetChatContentRes> getChatContentRes = chatRepository.getChatContent(roomId, id);
         return getChatContentRes;
+    }
+
+    @Override
+    public List<GetChatMessageRes> getChatMessages(String id) throws Exception {
+        List<GetChatMessageRes> getChatMessageRes = chatRepository.getChatMessages(id);
+        return getChatMessageRes;
     }
 
 
