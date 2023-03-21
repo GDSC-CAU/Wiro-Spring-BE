8ce1d208ac42bd671f8670783b21b9c02ee9fd72
Sangwoo Shin <sangwoonoel@naver.com>
2023-03-21 09:46:47 +0900

    Update ChatRepository: add getChatMessages method

--- src/main/java/google/solution/repository/ChatRepository.java
+++ src/main/java/google/solution/repository/ChatRepository.java
@@ -3,6 +3,7 @@ package google.solution.repository;
 
 import google.solution.domain.Message;
 import google.solution.dto.GetChatContentRes;
+import google.solution.dto.GetChatMessageRes;
 import google.solution.dto.GetChatRoomRes;
 import google.solution.dto.SendMessageRes;
 
@@ -13,4 +14,5 @@ public interface ChatRepository {
     public SendMessageRes sendMessage(String id, Message message) throws Exception;
     public GetChatRoomRes getChatRooms(String id) throws Exception;
     public List<GetChatContentRes> getChatContent(String roomId, String id) throws Exception;
+    public List<GetChatMessageRes> getChatMessages(String id) throws Exception;
 }
