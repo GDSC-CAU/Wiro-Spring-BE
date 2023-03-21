0716b95e0def0e37e41e09204e7d6a162a9a3050
Sangwoo Shin <sangwoonoel@naver.com>
2023-03-21 11:35:57 +0900

    Update GetChatMessageRes: change field name

--- src/main/java/google/solution/dto/GetChatMessageRes.java
+++ src/main/java/google/solution/dto/GetChatMessageRes.java
@@ -12,16 +12,16 @@ import lombok.Setter;
 @Setter
 public class GetChatMessageRes {
 
-    private String sourceId;
+    private String sourceNickname;
     private String content;
-    private String destinationId;
+    private String destinationNickname;
     private String updateTime;
 
     public static GetChatMessageRes messageToGetChatMessageRes(Message message) {
         GetChatMessageRes getChatMessageRes = new GetChatMessageRes();
-        getChatMessageRes.setSourceId(message.getSourceNickname());
+        getChatMessageRes.setSourceNickname(message.getSourceNickname());
         getChatMessageRes.setContent(message.getContent());
-        getChatMessageRes.setDestinationId(message.getDestinationNickname());
+        getChatMessageRes.setDestinationNickname(message.getDestinationNickname());
         getChatMessageRes.setUpdateTime(message.getUpdateTime());
         return getChatMessageRes;
     }
