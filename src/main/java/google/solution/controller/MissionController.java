a2c3626690b3031c6af9e667665a1ba3f6b1075b
Sangwoo Shin <sangwoonoel@naver.com>
2023-03-23 10:14:04 +0900

    Added MissionController: add getMissionInfo method

---
+++ src/main/java/google/solution/controller/MissionController.java
@@ -0,0 +1,26 @@
+package google.solution.controller;
+
+
+import google.solution.dto.GetChatRoomRes;
+import google.util.BaseResponse;
+import google.util.BaseResponseStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/mission")
+public class MissionController {
+
+    private final MissionService missionService;
+
+    @GetMapping("/getMissionInfo")
+    public BaseResponse<GetMissionInfoRes> getMissionInfo(GetMissionInfoReq getMissionInfoReq) {
+        try {
+            GetMissionInfoRes getChatRoomsRes = missionService.getMissionInfo(getMissionInfoReq.getCode());
+            return new BaseResponse<>(getChatRoomsRes);
+        } catch (Exception e) {
+            return new BaseResponse<>(BaseResponseStatus.FAIL);
+        }
+    }
+}
