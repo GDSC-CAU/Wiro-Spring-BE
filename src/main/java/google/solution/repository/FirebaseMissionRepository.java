2c554a9bf916952d8d6193fccf71abc946b9ba34
Sangwoo Shin <sangwoonoel@naver.com>
2023-03-23 11:51:45 +0900

    Update FirebaseMissionRepository: implement getSuccessMissions method

--- src/main/java/google/solution/repository/FirebaseMissionRepository.java
+++ src/main/java/google/solution/repository/FirebaseMissionRepository.java
@@ -4,9 +4,15 @@ import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.*;
 import com.google.firebase.cloud.FirestoreClient;
 import google.solution.domain.Mission;
+import google.solution.dto.GetChatContentRes;
 import google.solution.dto.GetMissionInfoRes;
+import google.solution.dto.MissionCompleteReq;
+import google.solution.dto.MissionCompleteRes;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Repository
@@ -32,5 +38,32 @@ public class FirebaseMissionRepository implements MissionRepository {
         else{
             return null;
         }
+    }
+
+    @Override
+    public List<MissionCompleteReq> getSuccessMissions(MissionCompleteReq missionCompleteReq) throws Exception {
+        List<MissionCompleteReq> successMissions = new ArrayList<>();
+        String id = Character.toString(missionCompleteReq.getCode().charAt(0));
+        String category = Character.toString(missionCompleteReq.getCode().charAt(1));
+        Firestore db = FirestoreClient.getFirestore();
+        CollectionReference missionCategory = db.collection(COLLECTION_NAME).document(id).collection(category);
+        Query query = missionCategory.orderBy("updateTime").limit(4);
+        ApiFuture<QuerySnapshot> future = query.get();
+        List<QueryDocumentSnapshot> missions = future.get().getDocuments();
+        for (QueryDocumentSnapshot mission : missions) {
+            MissionCompleteReq successMission = mission.toObject(MissionCompleteReq.class);
+            successMissions.add(successMission);
+        }
+
+        return successMissions;
+    }
+
+    @Override
+    public void saveScore(double score) throws Exception {
+    }
+
+    @Override
+    public MissionCompleteRes saveMissions(List<MissionCompleteReq> missions) throws Exception {
+        return null;
     }
 }
