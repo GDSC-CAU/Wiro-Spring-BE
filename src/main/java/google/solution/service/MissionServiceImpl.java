948fbd804164cd345ffc11c78ccec97fc7222046
Sangwoo Shin <sangwoonoel@naver.com>
2023-03-23 11:21:13 +0900

    Update MissionServiceImpl: implement missionComplete method

--- src/main/java/google/solution/service/MissionServiceImpl.java
+++ src/main/java/google/solution/service/MissionServiceImpl.java
@@ -2,10 +2,14 @@ package google.solution.service;
 
 import google.solution.domain.Mission;
 import google.solution.dto.GetMissionInfoRes;
+import google.solution.dto.MissionCompleteReq;
+import google.solution.dto.MissionCompleteRes;
 import google.solution.repository.MissionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -20,5 +24,18 @@ public class MissionServiceImpl implements MissionService {
         Mission mission = missionRepository.getMissionInfo(code);
         GetMissionInfoRes getMissionInfoRes = GetMissionInfoRes.missionToGetMissionInfoRes(mission);
         return getMissionInfoRes;
+    }
+
+    @Override
+    public MissionCompleteRes missionComplete(MissionCompleteReq missionCompleteReq) throws Exception {
+        List<MissionCompleteReq> missions = missionRepository.getSuccessMissions(missionCompleteReq);
+        if (missions.size() == 4) {
+            missions.add(missionCompleteReq);
+            calculateWeightedAverage(missions);
+            MissionCompleteRes missionCompleteRes = missionRepository.saveMissions(missions);
+        } else {
+            missions.add(missionCompleteReq);
+            MissionCompleteRes missionCompleteRes = missionRepository.saveMissions(missions);
+        }
     }
 }
