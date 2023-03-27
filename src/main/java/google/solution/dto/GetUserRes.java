4f811a10d6bfd39733904fad690336442b62e0c5
Sangwoo Shin <sangwoonoel@naver.com>
2023-03-27 13:16:24 +0900

    Update GetUserRes

--- src/main/java/google/solution/dto/GetUserRes.java
+++ src/main/java/google/solution/dto/GetUserRes.java
@@ -11,11 +11,19 @@ import lombok.Setter;
 public class GetUserRes {
 
     private String nickname;
+    private String blood;
+    private String disease;
+    private String medicine;
+    private String id;
 
     // User 객체를 GetUserRes로 변환
     public static GetUserRes userToGetUserRes(User user) {
         GetUserRes getUserRes = new GetUserRes();
         getUserRes.setNickname(user.getNickname());
+        getUserRes.setBlood(user.getBlood());
+        getUserRes.setDisease(user.getDisease());
+        getUserRes.setMedicine(user.getMedicine());
+        getUserRes.setId(user.getId());
         return getUserRes;
     }
 }
