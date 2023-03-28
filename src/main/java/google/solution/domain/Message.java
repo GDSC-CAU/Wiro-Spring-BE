3021381b0adf16354354e5c791b9ac85b84cbb8b
Sangwoo Shin <sangwoonoel@naver.com>
2023-03-28 12:40:55 +0900

    Update Message: add isFromMe field and add sendMessageReqToMessagemethod

--- src/main/java/google/solution/domain/Message.java
+++ src/main/java/google/solution/domain/Message.java
@@ -1,5 +1,6 @@
 package google.solution.domain;
 
+import google.solution.dto.SendMessageReq;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,4 +16,16 @@ public class Message {
     private String content;
     private String destinationNickname;
     private String updateTime;
+    private boolean isFromMe;
+
+    public static Message sendMessageReqToMessage(String sourceNickname, SendMessageReq sendMessageReq, boolean isFromMe) {
+        Message message = new Message();
+        message.setSourceNickname(sourceNickname);
+        message.setContent(sendMessageReq.getContent());
+        message.setDestinationNickname(sendMessageReq.getDestinationNickname());
+        message.setUpdateTime(sendMessageReq.getUpdateTime());
+        message.setFromMe(isFromMe);
+        return message;
+    }
 }
+
