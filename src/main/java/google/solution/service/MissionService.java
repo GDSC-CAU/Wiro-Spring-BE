f7ae2c6abdf54c60f38137d277ff7c28fa5681e0
Sangwoo Shin <sangwoonoel@naver.com>
2023-03-28 16:56:14 +0900

    Update MissionService: add getRecommendMission method

--- src/main/java/google/solution/service/MissionService.java
+++ src/main/java/google/solution/service/MissionService.java
@@ -2,10 +2,13 @@ package google.solution.service;
 
 import google.solution.dto.*;
 
+import java.util.List;
+
 public interface MissionService {
 
     public GetMissionInfoRes getMissionInfo(String id, String code) throws Exception;
     public MissionCompleteRes missionComplete(MissionCompleteReq missionCompleteReq, String userId) throws Exception;
     public GetMissionHistoryRes getMissionHistory(String userId) throws Exception;
     public GetCheckListHistoryRes getCheckListHistory(String userId) throws Exception;
+    public List<GetRecommendMissionRes> getRecommendMission(String userId) throws Exception;
 }
