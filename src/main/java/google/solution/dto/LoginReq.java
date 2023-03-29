198facea8f8ea78d74211be232dec5aa9681bbe7
Sangwoo Shin <sangwoonoel@naver.com>
2023-03-29 10:59:40 +0900

    Update User: add user information fields

--- src/main/java/google/solution/dto/LoginReq.java
+++ src/main/java/google/solution/dto/LoginReq.java
@@ -6,4 +6,8 @@ import lombok.Data;
 public class LoginReq {
     // 정보들 추가
     private String nickname;
+    private String blood;
+    private String disease;
+    private String id;
+    private String medicine;
 }
